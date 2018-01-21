ba238d2469443fba02dbe348ceddaf6389dfa9b8
Khoa A. Tran <khoa102@gmail.com>
2018-01-20 18:00:19 -0600

    Adding documentation for main.py and piece.py

--- main.py
+++ main.py
@@ -4,39 +4,59 @@ import sys
 
 
 class Game:
-    top = 'red'
-    bottom = 'black'
-    teams = ['black', 'red']
-    piece = ['chariot', 'horse', 'elephant', 'advisor', 'general', 'soldier', 'cannon']
-    chess_pcs_dist = {'soldier': 5, 'cannon': 2, 'chariot': 2, 'horse': 2, 'elephant': 2, 'advisor': 2, 'general': 1}
-    black_pieces = {}
-    red_pieces = {}
-    board = []
-    top_init_pos = {}
-    bottom_init_pos = {}
+    """
+            A class that represents a game of chess. This class has a few constant to initiate the game
+    """
+
+    PIECE = ['chariot', 'horse', 'elephant', 'advisor', 'general', 'soldier', 'cannon']
+    CHESS_PCS_DIST = {'soldier': 5, 'cannon': 2, 'chariot': 2, 'horse': 2, 'elephant': 2, 'advisor': 2, 'general': 1}
     BOARD_IS_EMPTY = "   .   "
 
     def __init__(self):
+        """
+            Initialize the game
+        """
+        # Declaring some nonstatic variable for class
+        self.black_pieces = {}    # The dictionary that contains all the black piece
+        self.red_pieces = {}      # The dictionary that contains all the red piece
+        self.board = []           # A 2D array that contains all the piece at the their current position
+        self.top = None
+        self.bottom = None
+        self.top_init_pos = {}
+        self.bottom_init_pos = {}
+
+        # Calling the function to initiate the two dictionaries and the board
         self.init_team()
         self.init_dict()
         self.generate_init_pos()
         self.init_piece_pos()
         self.init_board()
 
-    def init_team(self):
-        self.top = 'red'
-        self.bottom = 'black'
+    def init_team(self, top='red', bottom='black'):
+        """
+            Initialize the position of the two teams
+        :param top: The team on top
+        :param bottom: The team on bottom
+        """
+        self.top = top
+        self.bottom = bottom
 
     def init_dict(self):
-        for piece in self.piece:
-            for i in range(self.chess_pcs_dist[piece]):
+        """
+            Initialize the two dictionaries that contains the pieces of the two teams
+        """
+        for piece in self.PIECE:
+            for i in range(self.CHESS_PCS_DIST[piece]):
                 self.black_pieces[piece + '_' + str(i)] = Piece('black_' + piece + '_' + str(i), 'black', piece)
 
-        for piece in self.piece:
-            for i in range(self.chess_pcs_dist[piece]):
+        for piece in self.PIECE:
+            for i in range(self.CHESS_PCS_DIST[piece]):
                 self.red_pieces[piece + '_' + str(i)] = Piece('red' + piece + '_' + str(i), 'red', piece)
 
     def generate_init_pos(self):
+        """
+            Generate the initial position for the pieces on top and bottom side
+        """
         j = 0
         for i in range(0, 9, 2):
             self.top_init_pos['soldier_' + str(j)] = (3, i)
@@ -45,11 +65,11 @@ class Game:
 
         for i in range(0, 4):
             j = 0
-            self.top_init_pos[self.piece[i] + '_' + str(j)] = (0, i)
-            self.bottom_init_pos[self.piece[i] + '_' + str(j)] = (9, i)
+            self.top_init_pos[self.PIECE[i] + '_' + str(j)] = (0, i)
+            self.bottom_init_pos[self.PIECE[i] + '_' + str(j)] = (9, i)
             j += 1
-            self.top_init_pos[self.piece[i] + '_' + str(j)] = (0, 8-i)
-            self.bottom_init_pos[self.piece[i] + '_' + str(j)] = (9, 8-i)
+            self.top_init_pos[self.PIECE[i] + '_' + str(j)] = (0, 8-i)
+            self.bottom_init_pos[self.PIECE[i] + '_' + str(j)] = (9, 8-i)
 
         self.top_init_pos['general_0'] = (0, 4)
         self.bottom_init_pos['general_0'] = (9, 4)
@@ -59,6 +79,9 @@ class Game:
         self.bottom_init_pos['cannon_1'] = (7, 7)
 
     def init_piece_pos(self):
+        """
+            Initialize the position of the pieces in the two dictionaries based on the position of the team
+        """
         if self.top == 'red':
             for key in self.red_pieces.keys():
                 self.red_pieces[key].set_init_pos(self.top_init_pos[key])
@@ -70,6 +93,9 @@ class Game:
                 self.black_pieces[key].set_init_pos(self.top_init_pos[key])
 
     def print_pieces(self):
+        """
+            Print out the two dictionaries to debug
+        """
         for key in self.red_pieces.keys():
             print(key + ": " + tuple_to_str(self.red_pieces[key].get_current_pos()))
 
@@ -78,6 +104,9 @@ class Game:
             print(key + ": " + tuple_to_str(self.black_pieces[key].get_current_pos()))
 
     def init_board(self):
+        """
+            Initialize the the 2D board
+        """
         for i in range(0, 10):
             board_y = []
             for j in range(0, 9):
@@ -93,12 +122,20 @@ class Game:
             self.board[y][x] = self.black_pieces[key]
 
     def print_board(self):
+        """
+            Print out the 2D board
+        """
         for i in range(10):
             for j in range(9):
                 sys.stdout.write(self.board[i][j].__str__() + " ")
             print()
 
     def move(self, cur_pos, new_pos):
+        """
+            Move a piece to a new position
+        :param cur_pos: the new position that the piece will move to
+        :param new_pos: the position of the piece that will be moved
+        """
         piece = None
 
         if cur_pos in self.black_pieces:
@@ -114,6 +151,11 @@ class Game:
         self.update(piece, new_pos)
 
     def validate(self, piece, new_pos):
+        """
+            Validate if the move is valid or not
+        :param piece: The piece that is moved
+        :param new_pos: The new position that the piece will move to
+        """
         cur_pos = piece.get_current_pos()
         team = piece.team
         type = piece.type
@@ -148,6 +190,10 @@ class Game:
                 pass
 
     def update(self):
+        """
+            Updating the position of the piece if the move is validated
+        :return:
+        """
         pass
 
 
--- piece.py
+++ piece.py
@@ -1,5 +1,21 @@
 class Piece(object):
+    """
+        A class for storing information about a piece in the chess game. The attribute that needs to be store are:
+        name: Name of the piece
+        team: Team of the piece (black or red)
+        type: Type of the piece
+        his_pos: A list that stores all the moves of the piece
+        pass_river: A boolean value states whether the piece has passed the river of not (for soldier type)
+        nick_name: A nickname to display on the CLI board
+    """
     def __init__(self, name, team, piece_type):
+        """
+            A constructor for Piece class
+
+        :param name: The name of the piece
+        :param team: The team that the piece belongs to (black or red)
+        :param piece_type: The type of piece
+        """
         self.name = name
         self.team = team
         self.type = piece_type
@@ -8,16 +24,34 @@ class Piece(object):
         self.nick_name = team[0] + '_' + self.type[0:3] + '_' + self.name[-1]
 
     def set_init_pos(self, pos):
+        """
+            A method to initiate the pos of the piece. This is separate from the __init__() because of the way
+            the piece is initiated
+
+        :param pos: The initial position of the piece
+        """
         self.his_pos.append(pos)
 
     def move(self, pos):
+        """
+            A method to change the position of the piece
+
+        :param pos: the new position for the piece
+        """
         self.his_pos.append(pos)
 
     def get_current_pos(self):
+        """
+            A method to get the current position of the piece
+
+        :return: current position of the piece
+        """
         return self.his_pos[-1]
 
     def __str__(self):
+        """
+           A method to display the piece as a string
+        :return: The nickname to represent the piece
+        """
         return self.nick_name
 
-    def __repr__(self):
-        return self.name
